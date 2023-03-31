70a55f839eea657ddda0f481ca35a49c703b301d
Nguyễn Tú Anh <anhnt0112@its-global.vn>
2023-03-31 17:10:39 +0700

    31/3/2023

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,90 @@
 import Footer from "../components/Footer/index.jsx";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="root">
-      <h1 className="aa">Hiiii</h1>
-      <Footer />
+      {/* Header */}
+      <div className="header">
+        <div className="logo">Logo</div>
+        <div className="">
+          <ul className="top-bar">
+            <li>Home</li>
+            <li>Category</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Slider */}
+      <div className="desc-banner">
+        <ul className="list-category">
+          <li>
+            <Link href="/">aaaaaaaaaaaaaaa</Link>
+          </li>
+          <li>
+            <Link href="/">aaaaaaaaaaaaaaa</Link>
+          </li>
+          <li>
+            <Link href="/">aaaaaaaaaaaaaaa</Link>
+          </li>
+          <li>
+            <Link href="/">aaaaaaaaaaaaaaa</Link>
+          </li>
+          <li>
+            <Link href="/">aaaaaaaaaaaaaaa</Link>
+          </li>
+        </ul>
+        <div className="banner-slider">
+          <Swiper
+            spaceBetween={30}
+            centeredSlides={true}
+            autoplay={{
+              delay: 4500,
+              disableOnInteraction: false,
+            }}
+            // pagination={{
+            //   clickable: true,
+            // }}
+            navigation={true}
+            modules={[Autoplay, Pagination, Navigation]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img
+                src="/images/1.jpg"
+                alt="Picture of the author"
+                width={500}
+                height={500}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="/images/2.jpg"
+                alt="Picture of the author"
+                width={500}
+                height={500}
+              />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+
+      {/* Service Description */}
+
+      {/* <Footer /> */}
 
       {/* Hight Light Product */}
-      <div className="hightlight-product">
+      {/* <div className="hightlight-product">
         <div className="hightlight-product-container">
           <div className="hightlight-product__top">
             <div className="hightlight-product__top--left">
@@ -64,7 +141,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
